ebf79e821a2170f103c8f92214235fa938145824
Martin Egli <martin@eglinet.ch>
2019-12-27 16:38:05 +0100

    Update appveyor build

--- RunAppVeyor.cmd
+++ RunAppVeyor.cmd
@@ -3,5 +3,5 @@ rem ***************************************************************************
 rem * Build Script for Debug Run
 rem ***************************************************************************
 rem powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Verbosity Diagnostic
-powershell .\Tools\Resources\build.ps1 -Target AppVeyor
+powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Script Run.cake
 rem pause
