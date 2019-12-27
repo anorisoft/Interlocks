cf86adf3a4dfe8b707fed594faa2c10394422718
Martin Egli <martin@eglinet.ch>
2019-12-27 16:24:21 +0100

    Fix

--- RunAppVeyor.cmd
+++ RunAppVeyor.cmd
@@ -4,4 +4,4 @@ rem * Build Script for Debug Run
 rem ***************************************************************************
 rem powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Verbosity Diagnostic
 powershell .\Tools\Resources\build.ps1 -Target AppVeyor
-pause
+rem pause
