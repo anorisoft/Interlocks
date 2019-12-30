8232bd4f6e3304c56dc98622dcbeceb4810fa000
Martin <Martin@eglinet.ch>
2019-12-30 12:41:08 +0100

    Update Build Scrips

--- RunAppVeyor.cmd
+++ RunAppVeyor.cmd
@@ -1,9 +1,8 @@
 @echo off
 rem ***************************************************************************
-rem * Build Script for Debug Run
+rem * Build Script for AppVeyor Run
 rem ***************************************************************************
 rem powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Verbosity Diagnostic
 type Tools\GlobalSettings\Addins.cake
-powershell .\Tools\Resources\build.ps1 -Target Print-AppVeyor-Environment-Variables -Script RunNuGet.cake
-powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Script RunNuGet.cake -Verbosity Diagnostic
-rem pause
+powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Script RunNuGet.cake
+rem powershell .\Tools\Resources\build.ps1 -Target AppVeyor -Script RunNuGet.cake -Verbosity Diagnostic
