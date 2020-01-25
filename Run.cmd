66ea3e3ee0594b5bb4a63c83962ecfe2f370eb00
Martin <Martin@eglinet.ch>
2020-01-25 22:22:04 +0100

    Fix docFx PDF

--- Run.cmd
+++ Run.cmd
@@ -2,6 +2,6 @@
 rem ***************************************************************************
 rem * Build Script for Debug Run
 rem ***************************************************************************
-powershell .\Tools\Resources\build.ps1 -Script Run.cake -Verbosity Diagnostic -Configuration "Debug"
-rem powershell .\Tools\Resources\build.ps1 -Script Run.cake -Configuration "Debug"
+rem powershell .\Tools\Resources\build.ps1 -Script Run.cake -Verbosity Diagnostic -Configuration "Debug"
+powershell .\Tools\Resources\build.ps1 -Script Run.cake -Configuration "Debug"
 pause
